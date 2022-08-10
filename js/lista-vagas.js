f2f80720cb91f6aa8bb8d337916ec62c699b38f1
Lucas Alves <105066212+LuuucasDev@users.noreply.github.com>
2022-08-09 23:18:59 -0300

    Update lista-vagas.js

--- js/lista-vagas.js
+++ js/lista-vagas.js
@@ -1,10 +1,10 @@
-// Rota/Endpoint da API de Vagas
+
 let urlVagas = 'http://localhost:3000/vagas';
 
-// Elemento a ser "preenchido"
+
 const selectVagas = document.querySelector("#vaga");
 
-// Consumo de dados assíncrono a partir da API
+
 fetch(urlVagas)
     .then( resposta => resposta.json() )
     .then( dados => {
